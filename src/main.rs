4ba2c4e5347b7992aaafe1ae8a3641347decdc26
lukeisun <lukeisun57@gmail.com>
2023-12-11 21:14:01 -0500

    made submit own subcommand, will submit for part 2 if part 1, and made reqwest nonblocking

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use chrono::{self, Datelike};
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use dotenv::dotenv;
 use reqwest::{self, header};
 use select::document::Document;
@@ -8,23 +8,51 @@ use select::predicate::Name;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use std::process::{exit, Command};
+use std::process::Command;
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Cli {
-    #[arg(long)]
+    #[arg(long, global = true)]
     day: Option<u32>,
-    #[arg(long)]
+    #[arg(long, global = true)]
     year: Option<i32>,
-    #[arg(long, short)]
-    submit: Option<bool>,
-    #[arg(long, short)]
-    part: Option<u8>,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+#[derive(Subcommand)]
+enum Commands {
+    Submit {
+        #[arg(long, short)]
+        part: Option<u8>,
+    },
+    S {
+        #[arg(long, short)]
+        part: Option<u8>,
+    },
+}
+// Getting inputs
+async fn run(base_url: String, session_cookie: String) {
+    // dont care if it doesnt open
+    let _ = Command::new("xdg-open").arg(&base_url).output();
+    let mut headers = header::HeaderMap::new();
+    headers.insert("Cookie", session_cookie.parse().unwrap());
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+    println!("Writing files to cwd");
+    let _ = write_test_input_to_fs(&client, &base_url).await;
+    let _ = write_input_to_fs(&client, &base_url).await;
+    println!("Goodbye and good luck!");
 }
 // Problem Input
-fn write_input_to_fs(client: &reqwest::blocking::Client, base_url: &str) {
-    let resp = client.get(format!("{}/input", base_url)).send().unwrap();
-    let text = resp.text().unwrap();
+async fn write_input_to_fs(client: &reqwest::Client, base_url: &str) {
+    let resp = client
+        .get(format!("{}/input", base_url))
+        .send()
+        .await
+        .unwrap();
+    let text = resp.text().await.unwrap();
     let mut file = File::create("input.in").unwrap();
     file.write_all(text.as_bytes()).unwrap();
     println!("\tWrote input.in");
@@ -34,9 +62,9 @@ fn write_input_to_fs(client: &reqwest::blocking::Client, base_url: &str) {
 // Sometimes multiple examples given as well as visual examples.
 // I'll just write all code blocks to files.
 // and figure out which ones are valid as I'm working on it.
-fn write_test_input_to_fs(client: &reqwest::blocking::Client, base_url: &str) {
-    let resp = client.get(base_url).send().unwrap();
-    let text = resp.text().unwrap();
+async fn write_test_input_to_fs(client: &reqwest::Client, base_url: &str) {
+    let resp = client.get(base_url).send().await.unwrap();
+    let text = resp.text().await.unwrap();
     let document = Document::from(text.as_str());
     for (i, node) in document.find(Name("pre")).enumerate() {
         let file_name = format!("test_input{}.in", i);
@@ -45,10 +73,11 @@ fn write_test_input_to_fs(client: &reqwest::blocking::Client, base_url: &str) {
         println!("\tWrote {file_name}");
     }
 }
-fn submit(base_url: String, session_cookie: String, part: String) {
+// Submitting
+async fn submit(base_url: String, session_cookie: String, part: u8) {
     let mut headers = header::HeaderMap::new();
     headers.insert("Cookie", session_cookie.parse().unwrap());
-    let client = reqwest::blocking::Client::builder()
+    let client = reqwest::Client::builder()
         .default_headers(headers)
         .build()
         .unwrap();
@@ -56,35 +85,46 @@ fn submit(base_url: String, session_cookie: String, part: String) {
     let mut input = String::new();
     let stdin = std::io::stdin();
     let _input_size = stdin.read_line(&mut input);
-    let params = [("level", part.as_str()), ("answer", input.trim())];
+    let is_successful = post_answer(&url, &client, part.to_string(), &input).await;
+    if !is_successful && part == 1 {
+        println!("Seems like you might have already submitted part 1\nRetrying...");
+        let retry = post_answer(&url, &client, (part + 1).to_string(), &input).await;
+        if retry == false {
+            println!("Looks like you did both parts? Or something else is wrong :)");
+        }
+    }
+    println!("Finished Submitting!");
+}
+async fn post_answer(
+    url: &String,
+    client: &reqwest::Client,
+    part: String,
+    answer: &String,
+) -> bool {
+    let params = [("level", part.as_str()), ("answer", answer.trim())];
     println!("Submitting...");
-    let resp = client.post(url).form(&params).send().unwrap();
-    let text = resp.text().unwrap();
+    let resp = client
+        .post(url)
+        .form(&params)
+        .send()
+        .await
+        .expect("error submitting");
+    let text = resp.text().await.expect("error getting text");
     let document = Document::from(text.as_str());
     let main: Vec<Node> = document.find(Name("main")).collect();
     let submission_text = main[0].text();
     if submission_text.contains("That's the right answer!") {
         tree();
         println!("Nice you got it right!");
+    } else if submission_text.contains("You don't seem") {
+        return false;
     } else {
         println!("{submission_text}");
     }
+    true
 }
-fn run(base_url: String, session_cookie: String) {
-    // dont care if it doesnt open
-    let _ = Command::new("xdg-open").arg(&base_url).output();
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Cookie", session_cookie.parse().unwrap());
-    let client = reqwest::blocking::Client::builder()
-        .default_headers(headers)
-        .build()
-        .unwrap();
-    println!("Writing files to cwd");
-    write_input_to_fs(&client, &base_url);
-    write_test_input_to_fs(&client, &base_url);
-    println!("Goodbye and good luck!");
-}
-fn main() {
+#[tokio::main]
+async fn main() {
     let date = chrono::offset::Local::now();
     let cli = Cli::parse();
     let day = cli.day.unwrap_or(date.day());
@@ -96,15 +136,12 @@ fn main() {
     let base_url = format!("https://adventofcode.com/{}/day/{}", year, day);
     println!("\x1B[32mAdvent of Code Year {} - Day {}\x1B[0m", year, day);
     println!("{}", base_url);
-    if let Some(_) = cli.submit {
-        let part = cli.part.expect("No part provided");
-        if part != 1 && part != 2 {
-            println!("Part must be 1 or 2");
-            exit(1);
+    match &cli.command {
+        Some(Commands::Submit { part }) | Some(Commands::S { part }) => {
+            let part = part.unwrap_or(1);
+            let _ = submit(base_url, session_cookie, part).await;
         }
-        submit(base_url.clone(), session_cookie.clone(), part.to_string());
-    } else {
-        run(base_url.clone(), session_cookie.clone());
+        None => run(base_url, session_cookie).await,
     }
 }
 fn tree() {
